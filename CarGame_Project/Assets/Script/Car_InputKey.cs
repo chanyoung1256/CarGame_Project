092f2f4fd3ed98e6ba0b8af59f07d5a8700b987b
CHANYOUNKIM <hayoun124@naver.com>
2023-07-31 14:26:16 +0900

    move_wheel_animation

---
+++ CarGame_Project/Assets/Script/Car_InputKey.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Car_InputKey : MonoBehaviour
+{
+    //public Animator Fleft. Fright, Bleft, Bright;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
